3628495dcfe8f0f199038b81ef7793691ceb8701
421loop <a421loop@gmail.com>
2025-07-24 16:57:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-// Elements
-const permissionScreen = document.getElementById('permissionScreen');
-const mainApp = document.getElementById('mainApp');
+// Wait for DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Elements
+    const permissionScreen = document.getElementById('permissionScreen');
+    const mainApp = document.getElementById('mainApp');
+    const enableBtn = document.getElementById('enableBtn');
+    const recordBtn = document.getElementById('recordBtn');
 
-// Functions
-function startListening() {
-  permissionScreen.classList.add('hidden');
-  mainApp.classList.remove('hidden');
-  console.log('Listening started...');
-}
+    // Functions
+    function startListening() {
+        console.log('startListening called');
+        permissionScreen.classList.add('hidden');
+        mainApp.classList.remove('hidden');
+        console.log('Listening started...');
+    }
 
-function saveLast30Seconds() {
-  alert('Saved the last 30 seconds!');
-}
+    function saveLast30Seconds() {
+        console.log('saveLast30Seconds called');
+        alert('Saved the last 30 seconds!');
+    }
+
+    // Event listeners
+    enableBtn.addEventListener('click', startListening);
+    recordBtn.addEventListener('click', saveLast30Seconds);
+
+    console.log('RetroSound loaded successfully');
+    console.log('Elements found:', {
+        permissionScreen: !!permissionScreen,
+        mainApp: !!mainApp,
+        enableBtn: !!enableBtn,
+        recordBtn: !!recordBtn
+    });
+});
