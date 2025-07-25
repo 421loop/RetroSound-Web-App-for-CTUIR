776e23098ed08238e0d5c216da86dbe246d7179f
421loop <a421loop@gmail.com>
2025-07-24 17:02:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-// Wait for DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // Elements
-    const permissionScreen = document.getElementById('permissionScreen');
-    const mainApp = document.getElementById('mainApp');
-    const enableBtn = document.getElementById('enableBtn');
-    const recordBtn = document.getElementById('recordBtn');
+// Elements
+const permissionScreen = document.getElementById('permissionScreen');
+const mainApp = document.getElementById('mainApp');
 
-    // Functions
-    function startListening() {
-        console.log('startListening called');
-        permissionScreen.classList.add('hidden');
-        mainApp.classList.remove('hidden');
-        console.log('Listening started...');
-    }
+// Functions
+function startListening() {
+    console.log('startListening called');
+    permissionScreen.classList.add('hidden');
+    mainApp.classList.remove('hidden');
+    console.log('Listening started...');
+}
 
-    function saveLast30Seconds() {
-        console.log('saveLast30Seconds called');
-        alert('Saved the last 30 seconds!');
-    }
+function saveLast30Seconds() {
+    console.log('saveLast30Seconds called');
+    alert('Saved the last 30 seconds!');
+}
 
-    // Event listeners
-    enableBtn.addEventListener('click', startListening);
-    recordBtn.addEventListener('click', saveLast30Seconds);
-
-    console.log('RetroSound loaded successfully');
-    console.log('Elements found:', {
-        permissionScreen: !!permissionScreen,
-        mainApp: !!mainApp,
-        enableBtn: !!enableBtn,
-        recordBtn: !!recordBtn
-    });
-});
+console.log('RetroSound loaded successfully');
